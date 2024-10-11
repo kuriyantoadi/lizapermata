676852ce64fbf30b1f020e19c51c9131f8c02e28
kuriyantoadi <melatikuning212@gmail.com>
2024-10-12 02:22:35 +0700

    update ke awal

--- pages/super_admin/ajax/ajax_transaksi.php
+++ pages/super_admin/ajax/ajax_transaksi.php
@@ -36,23 +36,16 @@ if($_GET['action'] == "table_data"){
         }
         else {
            $search = $_POST['search']['value']; 
-        //    $query = $koneksi->query("SELECT * 
-        //                   FROM user AS usr 
-        //                   RIGHT JOIN transaksi AS trs ON trs.oleh = usr.id_user 
-        //                   LEFTJOIN tb_pelanggan AS plg ON trs.id_pelanggan = plg.id_pelanggan 
-        //                   WHERE trs.kode_keranjang LIKE '%$search%' 
-        //                      OR trs.total_harga LIKE '%$search%' 
-        //                   ORDER BY $order $dir 
-        //                   LIMIT $limit 
-        //                   OFFSET $start");
-
-             $query = $koneksi->query("SELECT * 
+           $query = $koneksi->query("SELECT * 
                           FROM user AS usr 
                           RIGHT JOIN transaksi AS trs ON trs.oleh = usr.id_user 
-                          LEFT JOIN tb_pelanggan AS plg ON trs.id_pelanggan = plg.id_pelanggan 
-                          ");
-
-                          
+                          LEFTJOIN tb_pelanggan AS plg ON trs.id_pelanggan = plg.id_pelanggan 
+                          WHERE trs.kode_keranjang LIKE '%$search%' 
+                             OR trs.total_harga LIKE '%$search%' 
+                          ORDER BY $order $dir 
+                          LIMIT $limit 
+                          OFFSET $start");
+              
            $querycount = $koneksi->query("SELECT count(id_transaksi) as jumlah FROM user AS usr RIGHT JOIN transaksi AS trs ON trs.oleh=usr.id_user WHERE trs.kode_keranjang LIKE '%$search%' OR trs.total_harga LIKE '%$search%'");
            $datacount = $querycount->fetch_array();
            $totalFiltered = $datacount['jumlah'];
